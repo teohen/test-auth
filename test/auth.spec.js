177adbbeb3069c04c28640e2743a9ac392b067e3
teohen <teodorohenriquecarvalho@gmail.com>
2020-12-21 14:41:44 -0300

    feat(auth-test): created more tests for the auth controller

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -2,6 +2,7 @@ const { expect, assert } = require('chai')
 const request = require('supertest')
 const faker = require('faker')
 const app = require('../src/app')
+const encrypt = require('../src/utils/encript')
 
 before(() => {
     userFake = {
@@ -12,29 +13,31 @@ before(() => {
 describe('Authentication test', () => {
     it('Should return a created status code when the valid requirements are provided', async () => {
         const response = await request(app).post('/user').send(userFake)
-
-        expect(response.status).eql(201)
+        assert.equal(response.status, 201, 'status code should be 201')
     })
 
     it('Should return a bad request status code when the password is not provided', async () => {
         const response = await request(app).post('/user').send({
             ...userFake, password: null
         })
-
-        expect(response.status).eql(400)
+        assert.equal(response.status, 400, 'Status should be 400')
     })
 
     it('Should return a bad request status code when the email is not provided', async () => {
         const response = await request(app).post('/user').send({
             ...userFake, email: null
         })
-
-        expect(response.status).eql(400)
+        assert.equal(response.status, 400, 'Status should be 400')
     })
     it('Should return a user with a token when the user is created', async () => {
         const response = await request(app).post('/user').send(userFake)
-        assert(response.body.token, "Missing property token in the response")
-        assert(response.body.email, "Missing property email in the response")
-        assert(response.body.password_hash, "Missing property password_hash in the response")
+        assert.exists(response.body.token, "response should have a token property")
+        assert.exists(response.body.email, "response should have a email property")
+        assert.exists(response.body.password_hash, "response should have a password_hash property")
+    })
+    it('Should return a correct hash for the provided password', async () => {
+        const hashed_pass = encrypt(userFake.password)
+        const response = await request(app).post('/user').send(userFake)
+        assert.equal(response.body.password_hash, hashed_pass, 'hashs should be equal')
     })
 })
